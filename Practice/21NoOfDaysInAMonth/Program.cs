37e777e7424ca00c58e173d68a00acf365b027dc
Vidyadhar Mudumbai <vidyadhar.mudumbai@campsystems.com>
2018-03-07 23:35:16 +0530

    New Programs

---
+++ Practice/21NoOfDaysInAMonth/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21NoOfDaysInAMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter the month number");
+            a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.WriteLine("The month has 31 days");
+                    break;
+                case 2:
+                    Console.WriteLine("The month has 28 days. It has 29 days if it is a leap year");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    Console.WriteLine("The month has 30 days");
+                    break;
+                default:
+                    Console.WriteLine("Enter valid month");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practice/22SumOfFirstTenNaturalNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22SumOfFirstTenNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, sum = 0;
+            for (j = 0; j <=10; j++)
+            {
+                sum = sum + j;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}

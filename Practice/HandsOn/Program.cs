ef6d8aca37f9f5341e9bc6ad2302cc17a696e5fa
Vidyadhar Mudumbai <vidyadhar.mudumbai@campsystems.com>
2018-03-07 23:11:12 +0530

    Pushing

---
+++ Practice/HandsOn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1>1 && num1<=100)
+           
+            else if (num1 < 35)
+            {
+                Console.WriteLine("The student is Pass");
+            
+            }
+            else
+            {
+                Console.WriteLine("The student failed");
+            }
+        }
+    }
+}
---
+++ Practice/HandsOnProg/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = { 40, 93, 56, 88, 75, 89 };
+            for (int i = 0; i < num1.Length; i++)
+            {
+                if (num1[i] < 35)
+                {
+                    Console.WriteLine("Failed");
+                    Console.ReadLine();
+                    break;                   
+                }
+
+                
+                    else  (num1[i] > 35)
+                        {
+                    Console.WriteLine("Pass");
+                }       
+
+                
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+

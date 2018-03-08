40553b2965b55aaf98a09b1b97a4b7ad7e1d9c5c
Vidyadhar Mudumbai <vidyadhar.mudumbai@campsystems.com>
2018-03-09 00:36:50 +0530

    Pushing Programs to Git

--- Practice/23SumAndAverage/Program.cs
+++ Practice/23SumAndAverage/Program.cs
@@ -10,6 +10,19 @@ namespace _23SumAndAverage
     {
         static void Main(string[] args)
         {
+            double avg;
+            int i, n, sum = 0;
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Num-{0} :",i);
+                n = Convert.ToInt32(Console.ReadLine());
+                sum = sum + n;
+            }
+            avg = sum / 10.0;
+            Console.WriteLine("Sum is " + sum);
+            Console.WriteLine("Average is " + avg);
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Practice/NOddNaturalNumbersAndTheirSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOddNaturalNumbersAndTheirSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, sum = 0;
+            Console.WriteLine("Enter the number of terms");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0} ", 2 * i - 1);
+                sum =  sum + (2 * i - 1);
+            }
+            Console.WriteLine("Sum is " + sum);
+            Console.ReadLine();
+        }
+    }
+}
